f8bc33898c17ecec2686e2758e352ee8c26000b4
fk <67616053+efendigufran@users.noreply.github.com>
2025-12-03 17:08:35 +0800

    Update app.js

--- app.js
+++ app.js
@@ -1,1 +1,26 @@
+// Tambah anggota
+function addMember() {
+  const name = document.getElementById("nameInput").value;
 
+  db.collection("members").add({
+    name: name,
+    saldo: 0
+  });
+
+  loadMembers(); 
+}
+
+// Tampilkan anggota
+function loadMembers() {
+  db.collection("members").onSnapshot(snapshot => {
+    const list = document.getElementById("memberList");
+    list.innerHTML = "";
+
+    snapshot.forEach(doc => {
+      const data = doc.data();
+      list.innerHTML += `<li>${data.name} — Saldo: Rp ${data.saldo}</li>`;
+    });
+  });
+}
+
+loadMembers();
