a7a4eefd190c48d846261666e3959334e93170c6
fk <67616053+efendigufran@users.noreply.github.com>
2025-12-03 16:46:34 +0800

    Update firebase.js
    memasukkan kode konfiugrasi firebase

--- firebase.js
+++ firebase.js
@@ -1,1 +1,9 @@
-
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCyT5p35LgWdtEG0aybT2OKbhxpuuJcUwk",
+  authDomain: "kas-badminton.firebaseapp.com",
+  projectId: "kas-badminton",
+  storageBucket: "kas-badminton.firebasestorage.app",
+  messagingSenderId: "229120749558",
+  appId: "1:229120749558:web:aeb33e8ea295088aace1d7"
+};
